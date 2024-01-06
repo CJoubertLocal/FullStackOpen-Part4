afe64522558309968b997cd03b401a97f3230707
CJoubertLocal <chrinnetwrok@gmail.com>
2024-01-06 15:53:03 +0000

    Reading of environment variables extracted into its own module utils/config.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-require('dotenv').config();
-
+const { PORT, MONGODB_URI } = require('./utils/config')
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
 
+mongoose.connect(MONGODB_URI);
+
+app.use(cors());
 app.use(express.json());
 
 const BlogRouter = require('./controllers/blogs');
 app.use('/api/blogs', BlogRouter);
 
-app.use(cors());
-app.use(express.json());
-
 const log = require('./utils/logger');
-const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   log.info(`Server running on port ${PORT}`);
 });
