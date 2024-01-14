a3a3fdd0fecb3d33cfa1be959d95d4b5612bd74c
CJoubertLocal <chrinnetwrok@gmail.com>
2024-01-14 23:47:02 +0000

    Exercises 4.13 & 4.14, added tests and async / await for updating and deleting blogs.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,7 +5,18 @@ blogRouter.get('/', async (request, response) => {
   const allBlogs = await Blog
     .find({});
 
-  response.json(allBlogs);
+  return response.json(allBlogs);
+});
+
+blogRouter.get('/:id', async (request, response) => {
+  try {
+    const blogToGet = await Blog
+      .findById(request.params.id);
+
+    return response.json(blogToGet);
+  } catch (exception) {
+    return response.status(400).json(exception);
+  }
 });
 
 blogRouter.post('/', async (request, response) => {
@@ -16,6 +27,45 @@ blogRouter.post('/', async (request, response) => {
       .save()
       .then((result) => {
         response.status(201).json(result);
+      });
+
+    return res;
+  } catch (exception) {
+    return response.status(400).json(exception);
+  }
+});
+
+blogRouter.put('/:id', async (request, response) => {
+  const update = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  };
+
+  try {
+    const res = await Blog
+      .findOneAndUpdate({ _id: request.params.id }, update, {
+        new: true,
+        runValidators: true,
+        context: 'query',
+      })
+      .then((updatedBlog) => {
+        response.json(updatedBlog);
+      });
+
+    return res;
+  } catch (exception) {
+    return response.status(400).json(exception);
+  }
+});
+
+blogRouter.delete('/:id', async (request, response) => {
+  try {
+    const res = await Blog
+      .findByIdAndDelete(request.params.id)
+      .then(() => {
+        response.status(204).end();
       });
 
     return res;
