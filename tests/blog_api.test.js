1d720f66321e8d41c4fd32bb56a7912f70c965f2
CJoubertLocal <chrinnetwrok@gmail.com>
2024-03-11 22:59:05 +0000

    Redid tests for exercises 4.9 and 4.10.

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -31,8 +31,52 @@ test('blogs are returned as json', async () => {
 
 test('get should return the correct number of blog posts', async () => {
   const response = await api.get('/api/blogs');
-
   assert.strictEqual(response.body.length, initialBlogs.listWithManyBlogs.length);
+});
+
+test('blog api should replace _id with id for returned results', async () => {
+  const response = await api.get('/api/blogs');
+  assert(Object.keys(response.body[0]).includes('id'));
+  assert(!Object.keys(response.body[0]).includes('_id'));
+});
+
+test('post to /api/blogs should create a new blog post', async () => {
+  const newBlog = {
+    title: 'A New Test Blog',
+    author: 'Some guy',
+    url: 'http://www.a-test-url.org/a-blog',
+    likes: 2,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const updatedBlogsList = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  assert.strictEqual(updatedBlogsList.body.length, initialBlogs.listWithManyBlogs.length + 1);
+
+  let newBlogFound = false;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < updatedBlogsList.body.length; i++) {
+    if (
+      newBlog.title === updatedBlogsList.body[i].title
+      && newBlog.author === updatedBlogsList.body[i].author
+      && newBlog.url === updatedBlogsList.body[i].url
+      && newBlog.likes === updatedBlogsList.body[i].likes
+    ) {
+      newBlogFound = true;
+    }
+  }
+
+  assert(newBlogFound);
 });
 
 after(async () => {
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -17,8 +17,10 @@ beforeEach(async () => {
   const initUsers = initialUsers
     .oneUser
     .map((user) => new User(user));
+
   const promiseArray = initUsers
     .map((user) => user.save());
+
   await Promise.all(promiseArray);
 });
 
@@ -33,6 +35,13 @@ test('get should return the correct number of users', async () => {
   const response = await api.get('/api/users');
 
   assert.strictEqual(response.body.length, initialUsers.oneUser.length);
+});
+
+test('user in test database should have the correct username and name', async () => {
+  const response = await api.get('/api/users');
+
+  assert.strictEqual(response.body[0].username, initialUsers.oneUser[0].username);
+  assert.strictEqual(response.body[0].name, initialUsers.oneUser[0].name);
 });
 
 after(async () => {
